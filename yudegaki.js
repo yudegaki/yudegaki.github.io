b81829c723c9cf87d39aeeb74de92804145081e8
yudegaki <42182478+yudegaki@users.noreply.github.com>
2018-09-25 02:55:57 +0900

    Add files via upload

--- yudegaki.js
+++ yudegaki.js
@@ -1,5 +1,5 @@
-var now_time=new Date();
-var check=now_time.getHours;
+var check_time=new Date();
+var check=check_time.getHours;
 var greet=document.getElementById("greeting");
 console.log(document.getElementById("greeting").textContent);
 /*時刻に応じて挨拶の変更をする*/
@@ -10,15 +10,20 @@ function greeting_func(){
 }
 greeting_func();
 /*現在時刻を表示するよ*/
-function LoadProc() {
+function Now_Clock() {
+    now_time=new Date();
     var target_time = document.getElementById("DateTimeDisp");
     var Year = now_time.getFullYear();
     var Month = now_time.getMonth()+1;
-    var Date = now_time.getDate();
+    var Dates = now_time.getDate();
     var Hour = now_time.getHours();
     var Min = now_time.getMinutes();
     var Sec = now_time.getSeconds();
-    target_time.textContent = "現在の時刻は"+Year + "年" + Month + "月" + Date + "日" + Hour + ":" + Min + ":" + Sec;
+    if(Hour>=0&&Hour<=9)Hour='0'+Hour;
+    if(Min>=0&&Min<=9)Min='0'+Min;
+    if(Sec>=0&&Sec<=9)Sec='0'+Sec;
+    target_time.innerHTML = "現在の時刻は"+Year + "年" + Month + "月" + Dates + "日" + Hour + ":" + Min + ":" + Sec;
 }
-LoadProc();
+Now_Clock();
+setInterval('Now_Clock()',1000,);
 
