4fc4519bef003449cb36fbdf034a477aef9ffdb9
yudegaki <42182478+yudegaki@users.noreply.github.com>
2019-01-01 07:15:03 +0900

    Update canvas.js

--- ronri-kairo/canvas.js
+++ ronri-kairo/canvas.js
@@ -1,30 +1,13 @@
 onload = function() {
-    draw();
-  };
-  function draw() {
-    /* canvas要素のノードオブジェクト */
-    var canvas = document.getElementById('hoge');
-    /* 2Dコンテキスト */
-    var ctx = canvas.getContext('2d');
-    /* 四角を描く */
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(1000,0);
-    ctx.lineTo(1000,750);
-    ctx.lineTo(0,750);
-    ctx.closePath();
-    ctx.stroke();
-    
-    for(var i=1;i<=4;i++){
-        canvas = document.getElementById('choice-'+i);
-        ctx = canvas.getContext('2d');
-        ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(200,0);
-        ctx.lineTo(200,200);
-        ctx.lineTo(0,200);
-        ctx.closePath();
-        ctx.stroke();
-    }
+  draw();
+};
+function draw() {
+  /* canvas要素のノードオブジェクト */
+  var canvas = document.getElementById('kairo-1');
+  //use_canvas(canvas.getContext('2d'),100,150);
+  for(var i=1;i<=4;i++){
+    canvas = document.getElementById('choice-'+i);
+    canvas.getContext('2d').strokeRect(0,0,1000,750);
   }
+}
 
