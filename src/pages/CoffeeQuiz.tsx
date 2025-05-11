0f243a70ebcccd3873096b705c19680dc6360d7a
yudegaki <kogakuin.shunsuke@gmail.com>
2025-05-11 20:59:52 +0900

    feat: delete StyleContainer

--- src/pages/CoffeeQuiz.tsx
+++ src/pages/CoffeeQuiz.tsx
@@ -1,18 +1,15 @@
-import Quiz from '@/components/quiz/quiz-start';
-import StyleContainer from '@/components/style-container';
+import Quiz from "@/components/quiz/quiz-start";
 
 const CoffeeQuizStartScreen = () => {
   return (
-    <StyleContainer>
-      <Quiz
-        quizTopic="coffeequiz"
-        title="コーヒーインストラクター検定2級 対策問題"
-        description={
-          'コーヒーインストラクター検定2級レベルのクイズを出題します。\n' +
-          '問題数は1~20問まで選択できます。'
-        }
-      />
-    </StyleContainer>
+    <Quiz
+      quizTopic="coffeequiz"
+      title="コーヒーインストラクター検定2級 対策問題"
+      description={
+        "コーヒーインストラクター検定2級レベルのクイズを出題します。\n" +
+        "問題数は1~20問まで選択できます。"
+      }
+    />
   );
 };
 
